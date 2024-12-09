4735a472b7ad4666cfdf9bf76cfa0d1f73546062
karan <zlac159@live.rhul.ac.uk>
2024-12-09 15:05:42 +0000

    Had to make big chnages to handle our new data format still needs bug fixing but mostly done

--- src/main/java/SoftParser.java
+++ src/main/java/SoftParser.java
@@ -6,65 +6,99 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 class parser{
-	//private String filename;
-	//public parser() {}
-	
-	//public parser(String filename) {
-	//	this.filename= filename; 
-	//}
 	private String line;//what we use to read the line in.
 	private String header;//to get the first line in.
 	private int count =0;
 	private List<String[]> data= new ArrayList<>();// this list stores a gene and it's expression vlaues.
+	private List<Entry> entries = new ArrayList<>();
 	
-	public List<String[]> softparser(String filename) throws IOException {
-		//lets try to first print the gene and it's id out 
-		
+	public List<Entry> softparser(String filename) throws IOException {
 		try {
 		BufferedReader br = new BufferedReader(new FileReader(filename));
-		// We'll try to find the very first row - count the number of samples and then store our expression values until then.
-		//We'll store the metadata later.
-		
 		while((header = br.readLine())!= null) {
 			if(!header.startsWith("^") && !header.startsWith("!") && !header.startsWith("#") ) {
 				break;//we'll skip the meta data lines.
 			}
 		}	
-		 // now header containes the first line of the column 
-		 Pattern samplePattern = Pattern.compile("GSM[0-9]+");//macthes to the gsm samples.
 		 String[] columns= header.split("\t");//splits the entries that we get, since they are tab delimited.
-			
-		 for (int i = 2; i < columns.length; i++) { // Skip first two columns id ref and gene
-	            if (samplePattern.matcher(columns[i]).matches()) {//gets the total number of gsm samples skipping the first two 
-	                count++;// this isn't getting incrementaed 
-	            }
-	        }
-		int numberOfSamples = count +2;
 		
+		 for(String temp : columns) {
+			 if(temp.startsWith("GSM")) {count++;}
+		 }
+		int numberOfSamples = count +2;//takes in first and second column
+		
+		Map<Integer, String> header_info= new HashMap<>();
+		for(int i =2; i<numberOfSamples;i++) {
+			header_info.put(i, columns[i]);//this works
+		}
+		//now we have the int -> header spec
+		//we want to have a gene -> expression for all of these header values
+		// the list of gene -> expression will be our entry and we want to do it for all the 
+		
+		//for each sample we make a hashmap
+		//iterate through that hashmap put column number 1()gene name -> the expression value.
+		// and then go to the next line 
+		 
+		List<Map<String, Double>> sampleData = new ArrayList<>(count);
+         for (int i = 0; i <=count; i++) {//we dont need to since count has skipped alreday
+             sampleData.add(new HashMap<>());
+         }//this creates gsm number of hashmap for us 
+         
+         while((line=br.readLine())!=null) {
+			if(line.startsWith("^")|| line.startsWith("!") ||line.startsWith("#") ) {
+				continue;//we'll skip the meta data lines.
+			}
+			String[] entries = line.split("\t");//splits the entries that we get, since they are tab delimited.
+			 String gene = entries[1]; // Gene name (second column)
+             for (int i = 2; i < numberOfSamples; i++) { // Start from the third column
+                 double expressionValue = Double.parseDouble(entries[i]);
+                 sampleData.get(i - 2).put(gene, expressionValue);
+             }
+		}
+		//this stores all the lines of the file -> we want to go until the max column and any amount of rows 
+		//so for each sample(i -> loop)
+		//make a hashmap
+		//then loop through the entries array 
+		//put entries(1)[gene name] and 
+		
+		int max = Collections.max(header_info.keySet());//gets the largest value for the key 
+		
+		for(int i=0; i<max;i++){//go through all the GSM samples
+			String GSMsample = header_info.get(i);
+			Map<String,Double> geneData = sampleData.get(i);
+			entries.add(new Entry(GSMsample,geneData));
+		}
+		
+		/*
 		while((line=br.readLine())!=null) {
 			if(line.startsWith("^")|| line.startsWith("!") ||line.startsWith("#") ) {
 				continue;//we'll skip the meta data lines.
 			}
 			String[] entries = line.split("\t");//splits the entries that we get, since they are tab delimited.
-			// we have the number of columns we have to parse - numberOfSamples
-			//find a way to get only the number of columns we need
+			
+			
+			
+			
 			
 			String[] geneData = new String[numberOfSamples];
 		    for (int i = 0; i < numberOfSamples; i++) {
 		        geneData[i] = entries[i]; // Copy only necessary columns
 		    }
 		    data.add(geneData); // Add the parsed row to the data list
-			
-		}
+			}*/
+		
 		br.close();
 		} catch (FileNotFoundException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
-	return data;
+	return entries;
 	}
 }
