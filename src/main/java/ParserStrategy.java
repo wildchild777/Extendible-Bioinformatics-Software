c2a81215b8098774c967ac9a1f4187b0c5b0fe9d
karan <zlac159@live.rhul.ac.uk>
2025-03-24 19:02:05 +0000

    Adding two interfaces so that we can have multiple type of parsed data being returned and multiple type of parsers that can run at runtime

---
+++ src/main/java/ParserStrategy.java
@@ -0,0 +1,11 @@
+
+/** This is an interface that applies the Strategy design pattern for our parsing needs.
+ */
+public interface ParserStrategy {
+/**
+ * @param filename the file to be parsed
+ * @return a type of parsedData 
+ */
+	public ParsedData parse(String filename);
+
+}
