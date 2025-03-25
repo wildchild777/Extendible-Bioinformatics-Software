058e0d92a3287df50b7e17c210b4adde214ba8d4
karan <zlac159@live.rhul.ac.uk>
2025-03-25 18:23:03 +0000

    Added Methods and test to return samples,sepcificSample mappin,and all gene expression with unit testing

--- src/main/java/GeneExpressionParsedData.java
+++ src/main/java/GeneExpressionParsedData.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This class is a type of data that holds gene expression data - like the ones you get if you parse SOFT files.
@@ -8,6 +10,8 @@ import java.util.List;
 public class GeneExpressionParsedData implements ParsedData {
 	
 	private List<Entry> entry = new ArrayList<Entry>();//where we store all the entries
+	private List<String> name= new ArrayList<String>();//where we store all the entries
+	private List<Map<String,Double>> gene = new ArrayList<Map<String,Double>>();//where we store all the entries
 	
 	public List<Entry>getEntries(){
 		return entry;
@@ -17,7 +21,29 @@ public class GeneExpressionParsedData implements ParsedData {
 		entry.add(temp);
 	}
 	
-//	public Entry getEntry() {
-		
-	//}
+	public List<String> getName(){
+		for ( Entry temp : entry) {
+			name.add(temp.getName());
+		}
+		return name;
+	}
+	
+	public List<Map<String,Double>> getGene(){
+		for ( Entry temp : entry) {
+			gene.add(temp.getGene());
+		}
+		return gene;
+	}
+	
+	public Map<String,Double> getSpecificGene(String sampleName) {
+		if(entry!=null) {
+			for(Entry t : entry) {
+				if (t.getName().equals(sampleName)) {
+					return t.getGene();
+				}
+			}
+		}
+		return null;
+	}
+
 }
--- src/test/java/ParsedDataTest.java
+++ src/test/java/ParsedDataTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,7 +10,8 @@ public class ParsedDataTest {
 	@Test
 	void setup() {
 		GeneExpressionParsedData temp = new GeneExpressionParsedData();
-		assertNull(temp.getEntries());
+		assertNotNull(temp.getEntries());
+		
 	} 
 	@Test
 	void addEntry(){
@@ -20,5 +22,31 @@ public class ParsedDataTest {
 		temp.add(hehe);//the entry we make here is null
 		assertNotNull(temp.getEntries());
 		
+	}
+	@Test
+	void get_all_sample_name() {
+		Map<String,Double> hello = new HashMap<String,Double>();//samples
+		hello.put("karan",5.0);
+		hello.put("arayn",7.0);
+		hello.put("mok",9.0);
+		hello.put("san",11.0);
+		GeneExpressionParsedData temp = new GeneExpressionParsedData();
+		for(int i =0; i<hello.size();i++) {
+			temp.add(new Entry("mewo"+i,hello));
+		}
+		assertNotNull(temp.getName());
+		assertNotNull(temp.getGene());
+		
+		Map<String,Double> gg= new HashMap<String,Double>();//samples
+		
+		
+		gg = temp.getSpecificGene("mewo2");//this will give me arayn -> 7.0
+		System.out.println(gg);
+		
+	}
+	@Test 
+	void get_null_for_exmpty_entrylist(){
+		GeneExpressionParsedData temp = new GeneExpressionParsedData();
+		assertNull(temp.getSpecificGene("gg"));
 	}
 }
