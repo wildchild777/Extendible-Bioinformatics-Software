5d9be3803922f4309d0869c635387822cac3e5c0
karan <zlac159@live.rhul.ac.uk>
2025-04-08 02:10:22 +0100

    This class allows for clients to ask for different parsers this is coupled with the Stratrgy pattern

---
+++ src/main/java/ParserContext.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+/**
+ * This class is used for requests and to pass the request onto concrete subclasses
+ */
+public class ParserContext {
+	//privately held strategy
+	private ParserStrategy strategy;
+	
+	/**
+	 * This is used to set the Parser We want to use
+	 * @param strategy - concrete Parsing algorithm we want to use
+	 */
+	public void setParser(ParserStrategy strategy) {
+        this.strategy = strategy;
+    }
+	
+	/**
+	 * This is used to execute the parse
+	 * @param filename - the file we want to parse
+	 * @return - returns ParsedData which can be further used
+	 */
+	public List<Entry> executeParse(String filename) {
+        if (strategy == null) {
+            throw new IllegalStateException("ParserStrategy is not set.");
+        }
+        return strategy.parse(filename);
+    }
+}
