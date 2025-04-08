4e5c4c0dbaa64d6d054235c280ba9d6fe025a9a1
karan <zlac159@live.rhul.ac.uk>
2025-04-08 02:06:30 +0100

    Dummy implementation of interfaces for Parsers and Clusters to return this dataType for now it's hardcoded

---
+++ src/main/java/ClusteredData.java
@@ -0,0 +1,7 @@
+/**
+ * This interface is used for classes that return data that has been worked on by 
+ * clustering algorithms
+ */
+public interface ClusteredData {
+	//need to make proper subclasses that implement this ClusteredData for k means etc
+}
--- src/main/java/ParsedData.java
+++ src/main/java/ParsedData.java
@@ -1,4 +1,3 @@
-import java.util.List;
 
 /**
  * This interface defines datatype that can be returned from a parse.
@@ -8,5 +7,5 @@ public interface ParsedData {
 	 * This returns the stored data from a parse - can be an concrete class.
 	 * @return a list containing a sample and it's gene - expression pairs.
 	 */
-	public List<Entry> getEntries();
+	//public List<Entry> getEntries();
 }
