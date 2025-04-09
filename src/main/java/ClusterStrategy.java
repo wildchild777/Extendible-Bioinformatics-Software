47aae3c077702c41a28f3ed10c74400527d7823f
karan <zlac159@live.rhul.ac.uk>
2025-04-09 02:36:41 +0100

    A strategy implementation for our clustering algo's

---
+++ src/main/java/ClusterStrategy.java
@@ -0,0 +1,18 @@
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This class implements the Strategy pattern to allow for dynamic switching between 
+ * different clustering alogrithms.
+ */
+public interface ClusterStrategy {
+	/**
+	 * Every clustering algorithm needs to return a type of ClusteredData (for now entry)
+	 * it runs on parsed data and returns the findings by running the ML algorithm.
+	 * @return the list of centroids (centre point) of entries 
+	 * -> entry(needs to be changed) with meaningful insights
+	 */
+	// this needs to return a proper subclass of the interface of CLusteredData
+	Map<Centroids, List<Entry>>fit(List<Entry> entry, int k, Distance distance, int maxIterations);
+}
