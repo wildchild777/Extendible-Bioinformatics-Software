b5acdb724b6cd7383a64420c98e006ad97775e13
karan <zlac159@live.rhul.ac.uk>
2025-04-10 14:56:46 +0100

    This view is how we will congifure the dataset algo and viz

---
+++ src/main/java/controller/WelcomeViewController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class WelcomeViewController {
+
+	@FXML
+	private Button NextButton;
+	private Stage stage;
+	
+	
+	public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+	void changeView(ActionEvent e) {
+		 try {
+			 FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/ChoiceView.fxml"));
+			 Parent root = loader.load();
+			 ChoiceViewController controller = loader.getController();
+	         controller.setStage(stage); // 🔁 pass it forward
+
+	         stage.setScene(new Scene(root, 800, 600));
+			
+         
+         } catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+}
