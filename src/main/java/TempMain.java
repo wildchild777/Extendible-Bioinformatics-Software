98762a934d4ce6ef7f7ca9668d4d1405f29f2838
karan <zlac159@live.rhul.ac.uk>
2025-04-12 19:22:55 +0530

    comments javadoc and controller passing

--- src/main/java/TempMain.java
+++ src/main/java/TempMain.java
@@ -2,6 +2,7 @@
 
 import java.io.IOException;
 
+import controller.*;
 import controller.WelcomeViewController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,17 +10,31 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
+import model.*;
+/**
+ * The start point of our application - bootstraps the controllers
+ */
 public class TempMain extends Application {
-
+/**
+ * Takes the stage and passes it onto the next view with all the controllers
+ */
     @Override
     public void start(Stage primaryStage) {
 	try {
+		//set up the initial contexts to be used 
+		ParserContext parserContext = new ParserContext();
+        ClusterContext clusterContext = new ClusterContext();
+        //manages the contexts - and sets up the main logic controller
+        ClusteringController clusteringController = new ClusteringController(parserContext, clusterContext);
+        	//basic FX
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/WelcomeView.fxml"));
 			Parent root = loader.load();
-			
+			/*Here we make the controller for the next view and pass all our controller as well
+			 * 
+			 * */
 			WelcomeViewController controller = loader.getController();
 			controller.setStage(primaryStage);
+			controller.setClusteringController(clusteringController);
 			
 			Scene scene = new Scene(root, 800, 600); // Width x Height
 	        primaryStage.setTitle("Clustering App");
@@ -34,5 +49,5 @@ public class TempMain extends Application {
     public static void main(String[] args) {
         launch(args); // Start JavaFX lifecycle
     }
-}
-
+} 
+ 
